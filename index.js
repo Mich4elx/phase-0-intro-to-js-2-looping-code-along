f30c8859ef3181ec5a72b1f0da35a4a786aa71f0
Michael Martinez <michaelm47x@gmail.com>
2021-12-09 21:32:43 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const happyBday = [];
+const bdays = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(bdays){
+    for (let i = 0; i<bdays.length; i++){
+        happyBday.push(`Thank you, ${bdays[i]}, for the wonderful surprise gift!`)
+        console.log(happyBday);
+    }
+    return happyBday;
+}
+
+function countDown(n){
+    while(n >= 0){
+        console.log(n)
+        n--
+    }
+    return n;
+}
+countDown(10);
